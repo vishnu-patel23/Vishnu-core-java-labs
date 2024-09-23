aa4b8a2b9d418f2cf4556db0334f7d2b17021378
Vishnu9549 <158544481+Vishnu9549@users.noreply.github.com>
2024-09-23 10:08:14 +0530

    3rd commit

--- src/com/deloitte/lab06/ex01/App.java
+++ src/com/deloitte/lab06/ex01/App.java
@@ -34,7 +34,6 @@ public class App{
         Collections.sort(valueList, new Comparator<Object>() {
             @Override
             public int compare(Object o1, Object o2) {
-                // Handle different types (if necessary)
                 if (o1 instanceof Comparable && o2 instanceof Comparable) {
                     return ((Comparable<Object>) o1).compareTo(o2);
                 }
